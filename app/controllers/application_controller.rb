09e232bb78b6df8891d14fbf4ab158c255ef1ccb
Leonardo Veras <andradeveras96@gmail.com>
2025-01-24 19:16:03 -0300

    Ajusta o Logout e cria um menu para todas as paginas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # Personalize o redirecionamento após o logout
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Redireciona para a página de login
+  end
 end
