745b97186084e37d9d07d6f8d4b6b6fcb75b1859
Leonardo Veras <andradeveras96@gmail.com>
2025-01-27 17:57:36 -0300

    Ajustando o teste de User
    Mudança necessária para garantir que a associação entre o user e o cart

--- spec/User/models/user_spec.rb
+++ spec/User/models/user_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe User, type: :model do
   context "associations" do
     it "has one cart" do
       user = User.create(email: "1@1.com", password: "123456", password_confirmation: "123456")
-      cart = Cart.create(user_id: user.id)
-      expect(user.cart).to eq(cart)
+      cart = Cart.create(user: user)
+
+      expect(user.cart.id).to eq(cart.id) # Verifica se o carrinho do usuário é o mesmo que o carrinho criado. 
+      expect(user.cart.user_id).to eq(user.id) # Verifica se o usuário do carrinho é o mesmo que o usuário criado.
+
     end    
   end
 end
