6b1adeb5a7a4362d75e4769476a2c78a6df6f924
Leonardo Veras <andradeveras96@gmail.com>
2025-01-27 17:39:14 -0300

    Adicionando comentários explicativos aos métodos do carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,11 +2,11 @@
 class CartsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
+  def show # Mostra o carrinho de compras do usuário logado no momento (current_user)  
     @cart = current_user.cart
   end
 
-  def add_item
+  def add_item # Adiciona um item ao carrinho de compras do usuário logado no momento (current_user)
     @cart = current_user.cart || current_user.create_cart
     @product = Product.find(params[:product_id])
     @cart_item = @cart.cart_items.find_or_initialize_by(product: @product)
@@ -16,14 +16,14 @@ class CartsController < ApplicationController
     redirect_to cart_path(@cart)
   end
 
-  def remove_item
+  def remove_item # Remove um item do carrinho de compras do usuário logado no momento (current_user)
     @cart = current_user.cart
     @cart_item = @cart.cart_items.find(params[:id])
     @cart_item.destroy
     redirect_to cart_path(@cart)
   end
 
-  def update_item
+  def update_item # Atualiza a quantidade de um item no carrinho de compras do usuário logado no momento (current_user)
     @cart = current_user.cart
     @cart_item = @cart.cart_items.find(params[:id])
     @cart_item.update(quantity: params[:quantity])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users, only: [:show]
 
   root 'products#index'
+  
+  devise_for :users
+
+  resources :users, only: [:show]
 
   resources :products, only: [:index, :show]
   
